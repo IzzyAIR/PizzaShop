0acc4feefbe8a5833ff4e0f267e69b0ceeef79f2
IzzyAIR <izzycode2105@gmail.com>
2022-10-04 23:57:24 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './scss/app.scss';
+import PizzaContext from './PizzaContext';
+
+import Header from './ReactComponets/Header';
+import Categories from './ReactComponets/Categories';
+import Sort from './ReactComponets/Sort';
+import PizzaBlock from './ReactComponets/PizzaBlock';
+
+
+
+function App() {
+	return (
+		<PizzaContext.Provider>
+			<div className='wrapper'>
+				<Header />
+				<div className='content'>
+					<div className='container'>
+						<div className='content__top'>
+							<Categories />
+							<Sort />
+						</div>
+						<h2 className='content__title'>Все пиццы</h2>
+						<div className='content__items'>
+							<PizzaBlock title='Чизбургер-пицца' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+							<PizzaBlock title='123' price='3500' imgUrl='' />
+						</div>
+					</div>
+				</div>
+			</div>
+		</PizzaContext.Provider>
+	);
+}
+
+export default App;
