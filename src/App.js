6e082bc89378402620a5d1e8a5f92723106ff23a
IzzyAIR <izzycode2105@gmail.com>
2022-10-06 20:10:28 +0500

    lesson 6-7.5

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import './scss/app.scss';
-import PizzaContext from './assets/PizzaContext';
 
 import Header from './ReactComponets/Header';
-import Categories from './ReactComponets/Categories';
-import Sort from './ReactComponets/Sort';
-import PizzaBlock from './ReactComponets/PizzaBlock';
-import pizzas from './assets/Pizza.json';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 function App() {
 	return (
-		<PizzaContext.Provider>
-			<div className='wrapper'>
-				<Header />
-				<div className='content'>
-					<div className='container'>
-						<div className='content__top'>
-							<Categories />
-							<Sort />
-						</div>
-						<h2 className='content__title'>Все пиццы</h2>
-						<div className='content__items'>
-							{pizzas.map((obj) => (
-								<PizzaBlock key={obj.id} {...obj} />
-							))}
-						</div>
-					</div>
-				</div>
+		<div className='wrapper'>
+			<Header />
+			<div className='content'>
+				<Home />
 			</div>
-		</PizzaContext.Provider>
+		</div>
 	);
 }
 
