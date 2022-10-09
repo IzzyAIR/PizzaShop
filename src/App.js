820ce91ac1822a46c984902771df4f8c7480a756
IzzyAIR <izzycode2105@gmail.com>
2022-10-09 19:42:00 +0500

    routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './scss/app.scss';
+import { Routes,  Route } from 'react-router-dom';
 
 import Header from './ReactComponets/Header';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 function App() {
 	return (
 		<div className='wrapper'>
 			<Header />
 			<div className='content'>
-				<Home />
+				<Routes>
+					<Route path='/' element={<Home/>}/>
+					<Route path='/cart' element={<Cart/>}/>
+					<Route path='*' element={<NotFound/>}/>
+				</Routes>
 			</div>
 		</div>
 	);
